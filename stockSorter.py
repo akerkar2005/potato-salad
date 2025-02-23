f80bcbd1f1c9b1d281b946f8ddfb0549c4dd9b45
pyaguega <143425717+pyaguega@users.noreply.github.com>
2025-02-23 02:03:55 -0500

    finbert hates potato salad

--- stockSorter.py
+++ stockSorter.py
@@ -1,71 +1,48 @@
 import pandas as pd
-BILLION = 10 ** 9
+preferences = {}
 
-class StockSorter:
+def customStockScore(stockData, preferences={}):
+    stockData = {COLUMNS[i]: stockData[i] for i in range(len(stockData))}
+    score = 0
 
-    def __init__(self, CSV_LOCATION = 'AGGREGATED_STOCK_DATA.csv'):
-        STOCK_DATA_DF = pd.read_csv(CSV_LOCATION)
-        STOCK_DATA_DF = STOCK_DATA_DF.fillna(0)
-        self.COLUMNS = STOCK_DATA_DF.columns.tolist()
-        STOCK_LIST = STOCK_DATA_DF.values.tolist()
+    if 'maxMarketCap' in preferences:
+        if stockData['marketCap'] <= preferences['maxMarketCap']:
+            score += 4
+    if 'minMarketCap' in preferences:
+        if stockData['marketCap'] >= preferences['minMarketCap']:
+            score += 4
 
-        self.STOCK_DATA = []
-        for row in STOCK_LIST:
-            stockData = {self.COLUMNS[i]: row[i] for i in range(len(row))}
-            self.STOCK_DATA.append(stockData)
+    if 'sector' in preferences: score += 10 if stockData['sector'] in preferences['sector'] else 0
+    if 'industry' in preferences: score += 10 if stockData['industry'] in preferences['industry'] else 0
 
+    goodShortRatio = preferences['shortRatio'] if 'shortRatio' in preferences else 2.0
+    badShortRatio = goodShortRatio * 2.0
+    if stockData['shortRatio'] <= goodShortRatio:
+        score += (max((goodShortRatio - stockData['shortRatio']) / 2, 2))
+    elif stockData['shortRatio'] >= badShortRatio:
+        score -= (stockData['shortRatio'] - badShortRatio) / 2
 
-    def customStockScore(self, stockData, preferences = {}):
-        
-        score = 0
+    goodCurrentRatio = preferences['currentRatio'] if 'currentRatio' in preferences else 1.5
+    score -= 2 * abs(stockData['currentRatio'] - goodCurrentRatio)
 
-        if 'maxMarketCap' in preferences:
-            if stockData['marketCap'] <= preferences['maxMarketCap']:
-                score += 10
-        if 'minMarketCap' in preferences:
-            if stockData['marketCap'] >= preferences['minMarketCap']:
-                score += 10
+    goodPERatio = preferences['forwardPE'] if 'forwardPE' in preferences else 25
+    badPERatio = goodPERatio * 2.0
+    if stockData['forwardPE'] <= goodPERatio and stockData['forwardPE'] > 0: 
+        score += 2
+    elif stockData['forwardPE'] >= badPERatio:
+        score -= (stockData['forwardPE'] - badPERatio) / 3
+    elif stockData['forwardPE'] <= 0:
+        score -= 15
 
-        if 'sector' in preferences: score += 10 if stockData['sector'] in preferences['sector'] else 0
-        if 'industry' in preferences: score += 15 if stockData['industry'] in preferences['industry'] else 0
-
-        goodShortRatio = preferences['shortRatio'] if 'shortRatio' in preferences else 2.0
-        badShortRatio = goodShortRatio * 2.0
-        if stockData['shortRatio'] <= goodShortRatio:
-            score += 2
-        elif stockData['shortRatio'] >= badShortRatio:
-            score -= (stockData['shortRatio'] - badShortRatio) / 2
-
-        goodCurrentRatio = preferences['currentRatio'] if 'currentRatio' in preferences else 1.5
-        score -= 2 * abs(stockData['currentRatio'] - goodCurrentRatio)
-
-        goodPERatio = preferences['forwardPE'] if 'forwardPE' in preferences else 10
-        badPERatio = goodPERatio * 2.0
-        if stockData['forwardPE'] <= goodPERatio and stockData['forwardPE'] > 0: 
-            score += 2
-        elif stockData['forwardPE'] >= badPERatio:
-            score -= (stockData['forwardPE'] - badPERatio) / 10
-        elif stockData['forwardPE'] <= 0:
-            score -= 5
-
-        return score
-
-    
-    def getUniqueIndustries(self):
-        industries = set()
-        for item in self.STOCK_DATA:
-            industries.add(item['industry'])
-
-        return list(industries)
-    
-    def getUniqueSectors(self):
-        sectors = set()
-        for item in self.STOCK_DATA:
-            sectors.add(item['sector'])
-
-        return list(sectors)
+    return score
 
 
-    def sortWithPreferences(self, preferences={}):
-        SORTED_DATA = sorted(self.STOCK_DATA, key=lambda stockData: self.customStockScore(stockData, preferences), reverse = True)
-        return SORTED_DATA
+NYSE_STOCK_DATA_DF = pd.read_csv('NYSE_DATA.csv')
+COLUMNS = NYSE_STOCK_DATA_DF.columns.tolist()
+
+NYSE_STOCK_DATA = NYSE_STOCK_DATA_DF.values.tolist()
+NYSE_STOCK_DATA.sort(key=lambda stockData: customStockScore(stockData, preferences), reverse = True)
+
+print(COLUMNS)
+for row in NYSE_STOCK_DATA[:5]:
+    print(row)

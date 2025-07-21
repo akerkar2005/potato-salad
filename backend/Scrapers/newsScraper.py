03e83c07de03528bb2291ba5225bd4e5890d897b
akerkar2005 <akerkar@purdue.edu>
2025-07-21 10:02:53 -0400

    just testing

--- backend/Scrapers/newsScraper.py
+++ backend/Scrapers/newsScraper.py
@@ -63,6 +63,6 @@ class NewsScraper:
                 print("ARTICLE TITLE: " + key)
                 print("DESCRIPTION: " + articlesDict[key])
                 print("------------------------------------------------------------------------")
-    
+
         return articlesDict
 
